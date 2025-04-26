eb6538f8ab6ad38a4d5c86ea85d4b945a422f138
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-04-26 23:51:27 +0300

    Initial Commit

---
+++ Notes App MVVM/App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_App_MVVMApp.swift
+//  Notes App MVVM
+//
+//  Created by Ahmet Bostanci on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("123456")
+        }
+    }
+}
